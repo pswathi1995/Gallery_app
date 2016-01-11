babcaf423f620aa7322877ca3116c41f03675782
ramyakonkala <ramya.konkala1995@gmail.com>
2016-01-11 21:42:00 +0530

    django forms with editable fields and update functionalities

---
+++ sample_app/gallery_app/views.py
@@ -0,0 +1,99 @@
+from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from .models import profile
+from django.conf import settings
+from django.http import HttpResponseRedirect
+
+from django.contrib import auth                 
+from django.core.context_processors import csrf 
+from forms import MyForm
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+
+def upload(request):
+   if request.method == 'POST':
+       form = MyForm(request.POST)     # create form object
+       print form.is_valid()
+       if form.is_valid():
+           name = request.POST.get('name')
+           pic = request.FILES.get('pic')
+           audio_track = request.FILES.get('audio_track')
+           video_track = request.FILES.get('video_track')
+           discription = request.POST.get('discription')
+           profile_obj = profile(name=name, pic=pic, discription=discription, audio_track=audio_track,  video_track=video_track)
+           profile_obj.save()
+           data = profile.objects.all()
+           return render(request, 'index.html', {"cont":data})
+       print form.errors
+       form = MyForm(request.POST)
+       variables = RequestContext(request, {
+           'error_message': form.errors,
+           'form' : form,
+       });
+       return render_to_response('upload.html', variables)
+   else:
+       args = {}
+       args.update(csrf(request))
+       args['form'] = MyForm()
+       print args
+  # return render_to_response('upload.html', variables)
+   return render(request, 'upload.html', args)
+
+def display(request):
+    data = profile.objects.all()
+    return render(request, 'index.html', {"cont":data})
+
+def uploadform(request):
+    return render(request,'upload.html')
+
+  
+from django.contrib import auth                 
+from django.core.context_processors import csrf 
+from forms import MyForm
+
+def register_user(request):
+    if request.method == 'POST':
+        form = MyForm(request.POST)     # create form object
+        print form.is_valid()
+        if form.is_valid():
+            print "ramya"
+            form.save()
+            return HttpResponseRedirect('display')
+    args = {}
+    args.update(csrf(request))
+    args['form'] = MyForm()
+    print args
+    return render(request, 'upload.html', args)
+
+
+def editform(request):
+    queryset = profile.objects.get(id = '34')
+    print "swathip"
+    if request.method == 'POST':
+        form = MyForm(request.POST,instance=queryset)
+        if form.is_valid():
+            print "ravaliK"
+            name = request.POST.get('name')
+            pic = request.FILES.get('pic')
+            print pic
+            audio_track = request.FILES.get('audio_track')
+            video_track = request.FILES.get('video_track')
+            discription = request.POST.get('discription')
+            profile.objects.filter(id = '34').update(name=name,pic=pic,audio_track=audio_track,video_track=video_track,discription=discription)
+            print "koganti"
+            data = profile.objects.all()
+            return render(request, 'index.html', {"cont":data})
+    else:
+        print "konkala"
+        form=MyForm(instance=queryset)
+        variables = RequestContext(request, {
+            'error_message': form.errors,
+            'form' : form,
+        });
+        
+        print "pantala"
+        return render_to_response('name.html', variables) 
+
